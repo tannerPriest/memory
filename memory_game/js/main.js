9e44ddd003c04e8506c9a302d591051f1e5b4e96
Tanner Priest <tannerpriest@Tanners-MBP.home>
2019-11-19 10:15:41 -0800

    created game  logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,1 +1,23 @@
-console.log ("connected");
+const cards = ["Queen", "Queen", "King", "King"];
+
+let cardsInPlay = [];
+
+var cardOne = cards[0];
+var cardTwo = cards[2];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+console.log("User Flipped " + cardOne +".");
+console.log("User Flipped " + cardTwo +".");
+
+
+
+if(cardsInPlay[0] === cardsInPlay[1]){
+	alert("A match!!");
+} else{
+	alert("Sorry, try again.");
+};
+
+
+
