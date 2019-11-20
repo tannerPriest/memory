6bfe8db052aaa40506c5a88370488d3dbab84bee
Tanner Priest <tannerpriest@Tanners-MBP.home>
2019-11-20 08:51:54 -0800

    Modified game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,16 +1,39 @@
-const cards = ["Queen", "Queen", "King", "King"];
+const cards = [
+	{
+		rank: "queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png"
+	},
+	{
+		rank: "queen",
+		suit: "diamonds",
+		cardImage: "images/queen-of-diamonds.png"
+	},
+	{
+		rank: "king",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png"
+	},
+	{
+		rank: "king",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds.png"
+	}
+
+];
 
 let cardsInPlay = [];
 
 function checkForMath(){
 	if(cardsInPlay[0] === cardsInPlay[1]){
-		console.log("You found a match!");
+		alert("You found a match!");
 	} else {
-		console.log("Sorry, try again.")
+		alert("Sorry, try again.")
 	};
 };
 
 function flipCard(cardId){
-    cardsInPlay.push(cards[cardId]);
-    console.log("User flipped " + cards[cardId]);
+    cardsInPlay.push(cards[cardId].rank);
+    console.log("User flipped " + cards[cardId].rank);
+    console.log(cardsInPlay);
 };
