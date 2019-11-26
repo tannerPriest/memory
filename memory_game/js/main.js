fc0d01ca74637b4fe0695f15b7312dd2a286a0b6
Tanner Priest <tannerpriest@Tanners-MacBook-Pro.local>
2019-11-25 20:00:11 -0800

    modified game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,55 +1,83 @@
 const cards = [
 	{
-		rank: "queen",
-		suit: "hearts",
-		cardImage: "images/Images/queen-of-hearts.png"
+		rank: 'queen',
+		suit: 'hearts',
+		cardImage: 'images/queen-of-hearts.png',
+		isFlipped: false
+	},
+	{   
+		rank: 'queen',
+		suit: 'diamonds',
+		cardImage: 'images/queen-of-diamonds.png',
+		isFlipped: false
 	},
 	{
-		rank: "queen",
-		suit: "diamonds",
-		cardImage: "images/Images/queen-of-diamonds.png"
+		rank: 'king',
+		suit: 'hearts',
+		cardImage: 'images/king-of-hearts.png',
+		isFlipped: false
+
 	},
 	{
-		rank: "king",
-		suit: "hearts",
-		cardImage: "images/Images/king-of-hearts.png"
-	},
-	{
-		rank: "king",
-		suit: "diamonds",
-		cardImage: "images/Images/king-of-diamonds.png"
+		rank: 'king',
+		suit: 'diamonds',
+		cardImage: 'images/king-of-diamonds.png',
+		isFlipped: false
 	}
 
 ];
 
 let cardsInPlay = [];
+  
 
 function createBoard(){
 	for(var i = 0; i < cards.length; i++){
 		var cardElement = document.createElement('img');
-		cardElement.setAttribute('src', 'images/Images/back.png');
-		cardElement.setAttribute('data-id', i)
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
 		document.getElementById('game-board').appendChild(cardElement);
 		cardElement.addEventListener('click', flipCard);
 	};
 };
-
 createBoard();
 
-function checkForMath(){
-	 cardElement.setAttribute('src', cards.cardImage);
+
+function checkForMath(cardElement){
 	// This is changing the card image to the correct card selected (king or queen).
-	if(cardsInPlay[0] === cardsInPlay[1]){
-		alert("You found a match!");
-	} else {
-		alert("Sorry, try again.")
+	if (cardsInPlay.length === 2) {
+	 if(cardsInPlay[0] === cardsInPlay[1]){
+		alert("You found a match! The game board will automatically reset.");
+		setTimeout(restart, 2500);
+	 } else {
+		alert("Sorry, try again. The game board will automatically reset.");
+		setTimeout(restart, 2500);
+		};
 	};
 };
 
-function flipCard(cardElement){
-    var cardId = this.getAttribute('data-id');
+function flipCard(){
+	var cardElement = this;
+    var cardId = cardElement.getAttribute('data-id');
+    if (cards[cardId].isFlipped) { return }
+    cardElement.setAttribute('src', cards[cardId].cardImage);
+    cards[cardId].isFlipped = true;
     cardsInPlay.push(cards[cardId].rank);
     console.log("User flipped " + cards[cardId].rank);
     console.log(cardsInPlay);
-    checkForMath();
+    checkForMath(cardElement);
 };
+
+function restart(){
+	for(var i = 0; i < cards.length; i++){
+		cardsInPlay.length = 0;
+		var x = document.querySelectorAll('img');
+		x[i].setAttribute('src', 'images/back.png');
+		cards[i].isFlipped = false;
+
+	};
+};
+
+
+
+
+
